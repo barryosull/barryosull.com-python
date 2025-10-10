c1629008da6bd9d61cb4bd69ddedd2bcdb1d565c
Barry O'Sullivan <bosulli85@gmail.com>
2025-10-11 00:09:58 +0100

    Role Overlay looks much better

--- frontend/src/components/RoleOverlay.jsx
+++ frontend/src/components/RoleOverlay.jsx
@@ -43,32 +43,34 @@ export default function RoleOverlay({ myRole, roomId, myPlayerId }) {
         <div className="role-description">
           {myRole.is_hitler && (
             <span>
-              You are Hitler! Work with the Fascists to get elected as Chancellor after 3 Fascist policies are enacted. 
+              You are Hitler! Work with the Fascists to get elected as Chancellor after 3 Fascist policies are enacted.
             </span>
           )}
           {!myRole.is_hitler && myRole.team === 'FASCIST' && (
             <span>
-              You are a Fascist! Work to enact Fascist policies and get Hitler elected as Chancellor. 
+              You are a Fascist! Work to enact Fascist policies and get Hitler elected as Chancellor.
             </span>
           )}
           {myRole.team === 'LIBERAL' && (
             <span>
-              You are a Liberal! Work to enact Liberal policies and prevent Hitler from being elected. 
-            </span>
-          )}
-          {myRole.teammates && myRole.teammates.length > 0 && (
-            <span>
-              {myRole.teammates.length === 1 ? 'Your Teammate:' : 'Your Teammates:'}
-              <ul>
-              {myRole.teammates.map((teammate) => (
-                <li key={teammate.player_id} className="teammate-name">
-                  {teammate.name} {teammate.is_hitler && '(Hitler)'}
-                </li>
-              ))}
-              </ul>
+              You are a Liberal! Work to enact Liberal policies and prevent Hitler from being elected.
             </span>
           )}
         </div>
+        {myRole.teammates && myRole.teammates.length > 0 && (
+          <div class="role-description">
+            <span>
+              {myRole.teammates.length === 1 ? 'Your Teammate:' : 'Your Teammates:'}
+            </span>
+            <ul className="team-mates">
+            {myRole.teammates.map((teammate) => (
+              <li className={teammate.is_hitler ? 'hitler' : 'fascist'} key={teammate.player_id}>
+                {teammate.name} {teammate.is_hitler ? '(Hitler)' : '(Fascist)'}
+              </li>
+            ))}
+            </ul>
+          </div>
+        )}
         <button onClick={handleClose} className="close-button">
           OK
         </button>
